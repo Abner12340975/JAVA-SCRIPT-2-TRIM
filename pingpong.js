bac78c771286dc37a3ece1687544831a449dbdbb
Abner Daniel <abner.casagrande@escola.pr.gov.br>
2024-07-25 17:12:27 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -1,23 +1,32 @@
-// aqui eu vou criar a bolinha
+//aqui eu vou criar a bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
-function setup (){
-    // aqui vou criar minha "mesa"
+
+//config velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
+
+function setup(){
+    //aqui vou criar minha "mesa"
     createCanvas(600,400);
 }
 
-function draw (){
-    // aqui vou por a cor da "mesa"
-    // A cor da mesa é em rgb
-    background (0,128,128);
-    // chamando a função
-
-    criaBolinha (xBolinha, yBolinha,tamBolinha);
+function draw(){
+    //aqui vou por a cor da "mesa"
+    //A cor da mesa é em rgb
+    background(128,128,0); 
+    //chamando a função
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
 }
 
+//função bolinha
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
+}
 
-// função bolinha
-function criaBolinha(xBolinha, yBolinha, tamBolinha){
-    circle (xBolinha, yBolinha, tamBolinha);
+//função move bolinha
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
 }
